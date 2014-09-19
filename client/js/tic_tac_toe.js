16459e4f10867022fe5b7bff6eedb0a818785410
Nikhil Handyal <nhandyal@gmail.com>
2014-09-19 01:54:15 -0700

    finished game rendering code

--- client/js/tic_tac_toe.js
+++ client/js/tic_tac_toe.js
@@ -53,3 +53,56 @@ Template.parent_game_grid.game_data = function() {
     return return_object;
 
 };
+
+var xo_targets_to_show = [];
+
+function process_xo_elements() {
+    for(var i = 0; i < xo_targets_to_show.length; i++) {
+        var xo_element_id = xo_targets_to_show.splice(0, 1);
+        $("#"+xo_element_id+" > .XO-container").addClass("full-opacity");
+    }
+}
+
+Template.parent_game_grid.helpers({
+    render_xo_element : function(element_index) {
+
+        var index = element_index,
+            target_id = '0' + element_index + 'P',
+            xo_element = this.parent_board[index].won;
+
+        xo_element = xo_element.toUpperCase();
+        if(xo_element == "X") {
+            xo_targets_to_show.push(target_id);
+            setTimeout(process_xo_elements, 0);
+            return "XP";
+        }else if(xo_element == "O") {
+            xo_targets_to_show.push(target_id);
+            setTimeout(process_xo_elements, 0);
+            return "OP";
+        }
+        
+        return "";
+    }
+});
+
+Template.child_game_grid.helpers({
+    render_xo_element : function(element_index) {
+        
+        var parent_index = this.index,
+            target_id = parent_index + '' + element_index + 'C',
+            xo_element = this.child_board[element_index];
+
+        xo_element = xo_element.toUpperCase();
+        if(xo_element == "X") {
+            xo_targets_to_show.push(target_id);
+            setTimeout(process_xo_elements, 0);
+            return "XC";
+        }else if(xo_element == "O") {
+            xo_targets_to_show.push(target_id);
+            setTimeout(process_xo_elements, 0);
+            return "OC";
+        }
+        
+        return "";
+    }
+});
