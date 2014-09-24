032b83a6d47282a48c7108d73ee7b50735f43a31
Nikhil Handyal <nhandyal@gmail.com>
2014-09-23 23:37:49 -0700

    added user registration

--- client/js/home.js
+++ client/js/home.js
@@ -1,3 +1,6 @@
+var email_input_id = "landing-input-email",
+    pwd_input_id = "landing-input-pwd";
+
 Template.home.helpers({
     show_login_register_container : function() {
         return typeof Session.get("landing_login_register_intent") !== "undefined";
@@ -5,7 +8,7 @@ Template.home.helpers({
 
     email_input_attributes : function() {
         return {
-            id : "landing-input-email",
+            id : email_input_id,
             class : "landing-text-input-element",
             name : "email",
             placeholder : "email",
@@ -19,11 +22,11 @@ Template.home.helpers({
 
     pwd_input_attributes : function() {
         return {
-            id : "landing-input-pwd",
+            id : pwd_input_id,
             class : "landing-text-input-element",
             name : "password",
             placeholder : "password",
-            type : "text",
+            type : "password",
             autocomplete : "off",
             required : "required"
         }
@@ -44,5 +47,66 @@ Template.home.events({
             Session.set("landing_login_register_intent", "register");
             TA.functions.push_window_history({}, "register", "/register");
         }
+    },
+
+    'click #landing-login-register-submit' : function(e) {
+        var email_regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            email = $.trim($("#"+email_input_id).val()),
+            pwd = $.trim($("#"+pwd_input_id).val()),
+            valid_email = email_regex.test(email),
+            valid_pwd = (pwd != ""),
+            intent = $.trim($("#landing-login-register-submit").html()).toLowerCase();
+
+        if(!valid_email) {
+            // flash the email field 3 times
+            (function() {
+                var toggle_count = 0,
+                    interval = setInterval(function() {
+                        $("#"+email_input_id).toggleClass("landing-color-alert");
+                        toggle_count++;
+                        if(toggle_count == 6) {
+                            clearInterval(interval);
+                        }
+                    }, 300);
+            })();
+            return;
+        }
+
+        if(!valid_pwd) {
+            // flash the pwd field 3 times
+            $("#"+pwd_input_id).val('');
+            (function() {
+                var toggle_count = 0,
+                    interval = setInterval(function() {
+                        $("#"+pwd_input_id).toggleClass("landing-color-alert");
+                        toggle_count++;
+                        if(toggle_count == 6) {
+                            clearInterval(interval);
+                        }
+                    }, 300);
+            })();
+        }
+
+        // passed input validation
+        if(intent == "register") {
+            Accounts.createUser({
+                username : chance.guid(),
+                email : email,
+                password : pwd,
+                profile : {}
+            }, function(error) {
+                if(error) {
+                    console.log(error);
+                    return;
+                }
+
+                console.log("created successfully");
+            });
+        }else if(intent == "login") {
+            console.log("login");
+        }else {
+            console.log("unknown intent");
+        }
+
     }
 });
--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 Meteor.startup(function () {
-    
-    
-
+    if((Meteor.users.find().count() == 0)) {
+        Meteor.users._ensureIndex(
+        	{ "email": 1},
+        	{ unique : true}
+        );
+    }
 });
